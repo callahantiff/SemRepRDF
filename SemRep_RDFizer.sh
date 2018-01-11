aef2d758089896ec8281bb4cd7e06f9fc46eb1ca
Tiffany J. Callahan <callahantiff@gmail.com>
2018-01-11 10:36:38 +0900

    edited comments

--- SemRep_RDFizer.sh
+++ SemRep_RDFizer.sh
@@ -5,6 +5,6 @@ if ! [[ -e README.md ]]; then
     exit 1
 fi
 
-# run python script to create a visual graph of each Clojure rule
+# run python script
 python /Users/tiffanycallahan/Dropbox/Papers-Conferences-Projects/Hackathons/BLAH 2018/SemRep_RDF.py -h "$path" -u
 "$path" -p "$path" -d "$path"
